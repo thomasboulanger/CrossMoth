3ec83bd52fd8e2c066abb644378ccecf5d3a6c46
Dorian Vabre <dorian.vabre@gmail.com>
2023-09-12 11:52:48 +0200

    controller with lights but no bounce

--- Assets/0_Scripts/MothController.cs
+++ Assets/0_Scripts/MothController.cs
@@ -1,7 +1,6 @@
 using UnityEngine;
 
-public class MothController : MonoBehaviour
-{
+public class MothController : MonoBehaviour {
     [Header("Debug Value")]
     [SerializeField] private string receivedString;
     [SerializeField] private string[] datas = new string[4];
@@ -19,32 +18,51 @@ public class MothController : MonoBehaviour
     private int _finalValueX;
     private int _finalValueZ;
 
-    private void Start()
-    {
+    [SerializeField] private GameObject haut;
+    [SerializeField] private GameObject bas;
+    [SerializeField] private GameObject gauche;
+    [SerializeField] private GameObject droite;
+
+    [SerializeField] private float bouncePower = 2f;
+    [SerializeField] private bool useBounce;
+
+    private void Start() {
         _rb = GetComponent<Rigidbody>();
         _serialController = GetComponent<SerialController>();
     }
 
-    private void Update()
-    {
+    private void Update() {
         receivedString = _serialController.ReadSerialMessage();
-        if(string.IsNullOrEmpty(receivedString) || receivedString is "__Connected__" or "__Disconnected__") return;
+        if (string.IsNullOrEmpty(receivedString) || receivedString is "__Connected__" or "__Disconnected__") return;
         datas = receivedString.Split(',');
-        
-        //receiving value order --> left/right/top/bottom
-        for (int i = 0; i < datas.Length; i++)
-        {
+
+        //receiving value order --> right/left/top/bottom
+        for (int i = 0; i < datas.Length; i++) {
             _incomeValues[i] = int.Parse(datas[i]);
             _incomeValues[i] = Mathf.Clamp(_incomeValues[i], minClampValue, maxClampValue);
         }
 
+        UpdateLights();
+
         _finalValueX = _incomeValues[0] - _incomeValues[1];
         _finalValueZ = _incomeValues[2] - _incomeValues[3];
-        if (_finalValueX < minimumMovementTreshold) _finalValueX = 0;
-        if (_finalValueZ < minimumMovementTreshold) _finalValueZ = 0;
-        
+        if (Mathf.Abs(_finalValueX) < minimumMovementTreshold) _finalValueX = 0;
+        if (Mathf.Abs(_finalValueZ) < minimumMovementTreshold) _finalValueZ = 0;
+
         //apply move values to directional vector
         _moveValue = new Vector3(_finalValueX, 0, _finalValueZ) * (Time.deltaTime * sensivity);
         _rb.AddForce(_moveValue);
+    }
+
+    void UpdateLights() {
+        droite.SetActive(_incomeValues[0] > minClampValue);
+        gauche.SetActive(_incomeValues[1] > minClampValue);
+        haut.SetActive(_incomeValues[2] > minClampValue);
+        bas.SetActive(_incomeValues[3] > minClampValue);
+    }
+
+    private void OnCollisionEnter(Collision collision) {
+        if (!useBounce) return;
+        _rb.AddForce(Vector3.Reflect(_rb.velocity, collision.contacts[0].normal) * bouncePower * _rb.velocity.magnitude);
     }
 }
