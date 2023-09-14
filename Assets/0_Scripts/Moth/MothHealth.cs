ec77e317e661e0e2afda90575d971ef62ed57a1a
Dorian Vabre <dorian.vabre@gmail.com>
2023-09-14 13:50:35 +0200

    back to menu on death

--- Assets/0_Scripts/Moth/MothHealth.cs
+++ Assets/0_Scripts/Moth/MothHealth.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MothHealth : MonoBehaviour
@@ -40,5 +41,6 @@ public class MothHealth : MonoBehaviour
 
     void Die() {
         Destroy(gameObject);
+        SceneManager.LoadScene("MainMenu");
     }
 }
